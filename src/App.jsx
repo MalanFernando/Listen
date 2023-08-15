63650cc74422d25246f2992777071efaf5cc5a99
Fernando <fernando-perugachi@hotmail.com>
2023-08-15 16:06:38 -0500

    Modified structure project to vite from CRA

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Practice from './components/Practice';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice />
+    </div>
+  );
+}
